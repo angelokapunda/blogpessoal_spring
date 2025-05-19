362d4f3aae40978c8f1357d7d2e5bbe75660272f
angelokapunda <angelokapunda@gmail.com>
2025-05-19 16:33:05 -0300

    :sparkles: CRUD(Create, Read, Update, Delete) adicionado no projeto

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -3,5 +3,9 @@ package com.generation.blogpessoal.repository;
 import com.generation.blogpessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+    List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 }
